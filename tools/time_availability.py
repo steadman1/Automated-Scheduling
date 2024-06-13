f0650655cc5b95feab536e7238f44cfb26db2377
steadman1 <steadman@vt.edu>
2024-06-13 13:56:39 -0400

    added SessionTimes.json for easy time changing and removed TimeBlock complexity

--- tools/time_availability.py
+++ tools/time_availability.py
@@ -1,20 +1,14 @@
-# parent class has longer name
-# bc it should be used less often
-# than the child "TimeBlock" class
-class TimeBlockAMPM:
-    def __init__(self, beginning, b_meridiem, end, e_meridiem) -> None:
-        self.beginning = beginning
-        self.b_meridiem = b_meridiem
+import json
+
+class TimeBlock:
+    def __init__(self, start, s_meridiem, end, e_meridiem) -> None:
+        self.start = start
+        self.s_meridiem = s_meridiem
         self.end = end
         self.e_meridiem = e_meridiem
         
     def __str__(self) -> str:
-        return f"{self.beginning}{self.b_meridiem if self.b_meridiem != self.e_meridiem else ""}-{self.end}{self.e_meridiem}"
- 
-# child class of "TimeBlockSwitching"
-class TimeBlock(TimeBlockAMPM):
-    def __init__(self, beginning, end, meridiem) -> None:
-        super().__init__(beginning, meridiem, end, meridiem)
+        return f"{self.start}{self.s_meridiem if self.s_meridiem != self.e_meridiem else ""}-{self.end}{self.e_meridiem}"
 
 class TimeAvailability:
     def __init__(self, text) -> None:
@@ -26,20 +20,18 @@ class TimeAvailability:
         """
         text = text.lower().replace(" ", "")
         
-        self.time_table = [
-            TimeBlock("10:00", "10:45", "am"),
-            TimeBlock("10:45",  "11:30", "am"),
-            TimeBlockAMPM("11:30", "am", "12:15", "pm"),
-            TimeBlock("12:15", "1:00", "pm"),
-            TimeBlock("1:00", "2:30", "pm"),
-            TimeBlock("2:30", "3:15", "pm"),
-            TimeBlock("3:15", "4:00", "pm"),
-            TimeBlock("4:00", "4:45", "pm"),
-            TimeBlock("4:45", "5:30", "pm"),
-            TimeBlock("5:30", "6:15", "pm"),
-            TimeBlock("6:15", "7:00", "pm"),
-        ]
+        self.time_table = []
+        for time in json.load(open("SessionTimes.json"))["session_times"]:
+            start = time["start"].replace(" ", "")
+            end = time["end"].replace(" ", "")
+            
+            s_meridiem = start[-2:].lower()
+            e_meridiem = end[-2:].lower()
+            
+            self.time_table.append(TimeBlock(start.replace(s_meridiem, ""), s_meridiem, end.replace(e_meridiem, ""), e_meridiem))
+        
         self.availability = [str(time) in text for time in self.time_table]
+        print(self.availability)
         
     def __str__(self) -> str:
         """
@@ -60,7 +52,7 @@ class TimeAvailability:
             if not index == 0:
                 string += "and "
             next_index, time_block = recursive_next_time(index)
-            string += f"from {self.time_table[indicies[index]].beginning}{self.time_table[indicies[index]].b_meridiem} to {time_block.end}{time_block.e_meridiem} "
+            string += f"from {self.time_table[indicies[index]].start}{self.time_table[indicies[index]].s_meridiem} to {time_block.end}{time_block.e_meridiem} "
             index += next_index + 1
     
         return string
